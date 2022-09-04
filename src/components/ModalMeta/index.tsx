5a52746acffd408799ea3cd1fc38e31a5b2a7b77
wendelbueres <wendelbueres@gmail.com>
2022-09-04 15:44:32 -0300

    feat: implementado Modal Metas

---
+++ src/components/ModalMeta/index.tsx
@@ -0,0 +1,102 @@
+import CurrencyInput from "react-currency-input-field";
+import { MdClear } from "react-icons/md";
+import { BtnSalvar, Input } from "../../styles/global";
+import { yupResolver } from "@hookform/resolvers/yup";
+import "./styles.css";
+import {
+  BotaoFechar,
+  BtnApagar,
+  ComponenteModal,
+  DivTitle,
+  FormModal,
+  InputsGroup,
+  LabelModal,
+  Modal,
+  Title,
+} from "./style.module";
+import { useForm } from "react-hook-form";
+
+interface IModal {
+  id?: string;
+  editar?: boolean;
+}
+
+interface FormValues {
+  description: string;
+  value: string | number;
+}
+
+export function ModalMeta({ id, editar }: IModal) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const onSubmit = handleSubmit((data) => {
+    let { value } = data;
+    console.log(data.value);
+  });
+
+  let tituloModal = "";
+  let botoes = <BtnSalvar>Salvar</BtnSalvar>;
+
+  if (editar) {
+    tituloModal = "Editar Meta";
+    botoes = (
+      <>
+        <BtnSalvar>Salvar</BtnSalvar>
+        <BtnApagar>Excluir</BtnApagar>
+      </>
+    );
+  } else {
+    tituloModal = "Criar Meta";
+  }
+
+  function fechaModal() {}
+
+  return (
+    <Modal>
+      <ComponenteModal>
+        <DivTitle>
+          <Title>{tituloModal}</Title>
+          <BotaoFechar onClick={() => fechaModal()}>
+            <MdClear />
+          </BotaoFechar>
+        </DivTitle>
+        <FormModal onSubmit={onSubmit}>
+          <InputsGroup>
+            <LabelModal htmlFor="objetivo">Objetivo</LabelModal>
+            <Input
+              type="text"
+              id="objetivo"
+              placeholder="Insira a descrição do objetivo"
+              {...register("description")}
+            />
+          </InputsGroup>
+          <InputsGroup>
+            <LabelModal htmlFor="valor">Valor</LabelModal>
+            <CurrencyInput
+              id="valor"
+              placeholder="R$"
+              decimalsLimit={2}
+              maxLength={18}
+              disableAbbreviations
+              defaultValue={0}
+              intlConfig={{ locale: "pt-BR", currency: "BRL" }}
+              {...register("value", {
+                setValueAs: (value) => {
+                  value = value.slice(3);
+                  value = value.replaceAll(".", "");
+                  value = value.replace(",", ".");
+                  value = parseFloat(value);
+                  return value;
+                },
+              })}
+            />
+          </InputsGroup>
+          <div id="botoes">{botoes}</div>
+        </FormModal>
+      </ComponenteModal>
+    </Modal>
+  );
+}
