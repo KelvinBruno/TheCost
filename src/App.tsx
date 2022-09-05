cdb8056cebc5ff85f8de1457667fcc127512c0b2
wendelbueres <wendelbueres@gmail.com>
2022-09-05 10:07:34 -0300

    feat: adicionados toastify e rotas ao app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Slide, ToastContainer } from "react-toastify";
 import Global from "./styles/global";
-import Login from "./components/Login";
+import { Router } from "./router";
 
 function App() {
   return (
     <div className="App">
       <Global />
-      <Login />
+      <Router />
+      <ToastContainer
+        position="top-right"
+        autoClose={3500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        transition={Slide}
+        pauseOnHover
+      />
     </div>
   );
 }
