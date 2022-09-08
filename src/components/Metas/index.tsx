98056c699427a0237ab41a468b415992e6d905e1
wendelbueres <wendelbueres@gmail.com>
2022-09-08 04:39:10 -0300

    fix: modal metas

--- src/components/Metas/index.tsx
+++ src/components/Metas/index.tsx
@@ -1,23 +1,58 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MetaContext } from "../../Contexts/MetasContext";
 import { IsOpenModalContext } from "../../Contexts/ModalContext";
 import { BtnEditarMeta, BtnRegistroMeta, Meta } from "./style.module";
 
 function Metas() {
-  const { metas } = useContext(MetaContext);
-  const { OpenModalEditMeta, OpenModalMeta, setOpenModalEditMeta, setOpenModalMeta, setDataMeta, setIdMeta} = useContext(IsOpenModalContext)
+  const { metas, carregaMeta } = useContext(MetaContext);
+  const {
+    OpenModalEditMeta,
+    OpenModalMeta,
+    setOpenModalEditMeta,
+    setOpenModalMeta,
+    setDataMeta,
+    setIdMeta,
+  } = useContext(IsOpenModalContext);
+  const [metasLista, setMetasLista] = useState(metas);
+
+  carregaMeta();
+  useEffect(() => {
+    setMetasLista(metas);
+  }, [metas]);
 
   return (
     <>
-      {metas.length > 0 ? (
+      {metasLista.length === 0 ? (
+        <Meta>
+          <div>
+            <h2>Meta</h2>
+
+            <BtnRegistroMeta
+              onClick={() => {
+                setOpenModalMeta(!OpenModalMeta);
+              }}
+            >
+              Nova Meta
+            </BtnRegistroMeta>
+          </div>
+        </Meta>
+      ) : (
         metas.map((item) => {
           const porcentagem = item.done / (item.value / 100);
 
           return (
-            <Meta key={item.id}>
+            <Meta>
               <div className="centralize-header-metas">
                 <h2>Meta</h2>
-                <BtnEditarMeta onClick={() => {setDataMeta(item); setIdMeta(item.id); setOpenModalEditMeta(!OpenModalEditMeta)}}>Editar Meta</BtnEditarMeta>
+                <BtnEditarMeta
+                  onClick={() => {
+                    setDataMeta(item);
+                    setIdMeta(item.id);
+                    setOpenModalEditMeta(!OpenModalEditMeta);
+                  }}
+                >
+                  Editar Meta
+                </BtnEditarMeta>
               </div>
               <div className="centralize-metas">
                 <h3>Objetivo: {item.objetivo} </h3>
@@ -40,14 +75,6 @@ function Metas() {
             </Meta>
           );
         })
-      ) : (
-        <Meta>
-          <div>
-            <h2>Meta</h2>
-
-            <BtnRegistroMeta onClick={() => {setOpenModalMeta(!OpenModalMeta)}}>Nova Meta</BtnRegistroMeta>
-          </div>
-        </Meta>
       )}
     </>
   );
